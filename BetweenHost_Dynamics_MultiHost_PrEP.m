34249fd135f666c26f37c7e650eb1efabf3e4d2c
SimonCalver <simon.calver@pmb.ox.ac.uk>
2016-05-20 15:31:01 +0100

    Add files via upload
    Has working VL stuff, I think

--- BetweenHost_Dynamics_MultiHost_PrEP.m
+++ BetweenHost_Dynamics_MultiHost_PrEP.m
@@ -20,11 +20,14 @@ spvlmax = 6;           % maximum log(spvl)
 forward_mut = 5e-5;    % Mutation rate of mutating to the next strain (index 1 higher)
 back_mut = 5e-5;       % Mutation rate of mutating into a strain with index 1 lower (NOTE: For non-neutral evolution we set forward_mut = back_mut)
 rand_mut = 0;          % Small mutation rate, mutation rate of mutating directly to a random other strain
-ART = 2;%1.3;               % After ART years the host goes onto ART (i.e. is removed from the susceptible population)
+ART = 5.0;               % After ART years the host goes onto ART (i.e. is removed from the susceptible population)
 
 Hprops = [1.0 0.0 0.0];   % proportions of hosts of each type (must add to 1)
-Drug_Adherance = {@(t) 0 @(t) concentration(t,0/365,0.5,48/8760,0,1.0) @(t) concentration(t,14/365,0.5,48/8760,0,1.0)};  %concentration(t,0.02,0.8,0,1);
-                  
+Drug_Adherance = {@(t) 0  @(t) concentration(t,0/365,0.5,48/8760,0,1.0)  @(t) concentration(t,36/365,0.5,48/8760,0,1.0)};  %concentration(t,0.02,0.8,0,1);
+% Here's a thought; what if the amount of drug depends on wether or not the person is infected? so one determines their infectvity and the other how easily they can be infected                    
+Drug_Adherance_Uninfected = {@(t) 0  @(t) 1.0  @(t) (t >= 36)};  %concentration(t,0.02,0.8,0,1);
+
+
 g = cell(nh,1);
 
 for i = 1:nh
@@ -47,7 +50,7 @@ tmax = 200;
 % Probability that newly infected cells become latent, vector for three stages (acute, asymptomatic, AIDS). [Note: in this work we do not change the values of parameters over the three stages]                  
 k = 0.0051*[1,1,1];%  0.016*[1,1,1];%   0.0051
 % Activation rate of latent cells (per day), vector for three stages (acute, asymptomatic, AIDS).
-a = 0.02*[1,1,1];%    0.003*[1,1,1];% 1e-2  
+a = 0.01*[1,1,1];%    0.003*[1,1,1];% 1e-2  
 % Size ratio between latent and active compartment: (size of latent reservoir)/(size of active compartment)
 rL = 2.0*[1,1,1];%  0.95*[1,1,1];%   2
 
@@ -59,7 +62,7 @@ D1 = 0.25-D0;                                           % Duration of acute infe
 D2 = (ART-D1-D0);                                      % Host goes onto ART 
 
 % Multiply these up to make R0 larger
-duh = 1;
+duh = 1.8;
 lambda0 = duh * 2.76;                                     % Infectiousness in first weeks (what should this be)
 lambda1 = duh * 2.76;                                     % Infectiousness during acute stage
 lambda2 = duh * Hill_func_up(10.^spvl,0.317,13938,1.02);  % Vector of infectiousness during asymptomatic phase
@@ -133,27 +136,27 @@ for i = 1:nh
     end
 end
 
-for l = 1:nh
-    for j = 1:nh
-        for i = 1:ns
-            for k = 1:ns
-                VL{l,j}(k,1:tau0(i),i) = lambda0 * host_x{j}(k,1:tau0(i),i);
-                VL{l,j}(k,(tau0(i)+1):tau1(i),i) = lambda1 * host_x{j}(k,(tau0(i)+1):tau1(i),i);
-                VL{l,j}(k,(tau1(i)+1):tau2(i),i) = lambda2(i) * host_x{j}(k,(tau1(i)+1):tau2(i),i);
+for i1 = 1:nh
+    for i2 = 1:nh
+        for j1 = 1:ns
+            for j2 = 1:ns
+                VL{i1,i2}(j2,1:tau0(j1),j1) = lambda0 * host_x{i2}(j2,1:tau0(j1),j1);
+                VL{i1,i2}(j2,(tau0(j1)+1):tau1(j1),j1) = lambda1 * host_x{i2}(j2,(tau0(j1)+1):tau1(j1),j1);
+                VL{i1,i2}(j2,(tau1(j1)+1):tau2(j1),j1) = lambda2(j1) * host_x{i2}(j2,(tau1(j1)+1):tau2(j1),j1);
             end
         end
     end
-end    
+end   
 
 % Strain 1 VL is reduced by the drug, so it is ~0 when a lot of drug, when
 % initiated by either strain. It won't go completely to 0 (andif it did
 % there would be NaNs in the solution). When infected initiLLY WITH STRAIN
 % 2, the VL follows the dynnamics in the absence of drug
-for i = 1:nh
-    for j = 1:nh
-       VL{i,j}(1,1:end,1) =  max(VL{i,j}(1,1:end,1) .* (1 - Drug_Adherance{j}(tt)), 0.001);
-    end
-end
+% for i = 1:nh
+%     for j = 1:nh
+%        VL{i,j}(1,1:end,1) =  max(VL{i,j}(1,1:end,1) .* (1 - Drug_Adherance{j}(tt)), 0.001);
+%     end
+% end
 % % Lets say after 30 days the VL of the resistant strain reaches the spvl,
 % % since there is definitely some present 
 % tStart = find(Drug_Adherance{i}(tt) >= 0, 1);
@@ -169,49 +172,62 @@ end
 % maybe? Should be quite low anyway cause of the low freq
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These are essentially the x(1-P) bits, but it is assumed that P = 1 for
+% all time unless the person gets infected
     VL{2,1}(2,1:end,1) =  min(VL{2,1}(2,1:end,1), 1e-20);
     VL{2,1}(2,1:end,2) =  min(VL{2,1}(2,1:end,2), 1e-20);
 % Host 1 (not on PrEP) should not be able to transmit virus to someone on
-% PrEP 
+% PrEP (because host 2 is on PrEP)
     VL{2,1}(1,1:end,1) =  min(VL{2,1}(1,1:end,1), 1e-20);
     VL{2,1}(1,1:end,2) =  min(VL{2,1}(1,1:end,2), 1e-20);
+% If host 2 stops PrEP after getting the virus then WT increases but we 
+% still assume that the hosts who don't have the virus remain on PrEP
+    VL{2,2}(1,1:end,1) =  min(VL{2,2}(1,1:end,1), 1e-20);
+    VL{2,2}(1,1:end,2) =  min(VL{2,2}(1,1:end,2), 1e-20);
+ 
     
     % How infectious WT is to host 3 depends on how much drug they have in
     % system 
     VL{3,1}(1,1:end,1) = max(VL{3,1}(1,1:end,1) .* (1 - Drug_Adherance{3}(tt)), 0.001);
     VL{3,1}(1,1:end,2) = max(VL{3,1}(1,1:end,2) .* (1 - Drug_Adherance{3}(tt)), 0.001);
  
+    VL{3,2}(1,1:end,1) = max(VL{3,2}(1,1:end,1) .* (1 - Drug_Adherance{3}(tt)), 0.001);
+    VL{3,2}(1,1:end,2) = max(VL{3,2}(1,1:end,2) .* (1 - Drug_Adherance{3}(tt)), 0.001);
+    
+    
 % Does this need to be chnged so that resistant is less infectious when they off the drug 
 % VL{3,2}(:,1:length(tt),2)
 
-% Incorporate the natural mortality
-for l = 1:nh
+% We assume the infectvity of the WT strain (1) is further altered by the
+% concentration of drug in the recipient host, using the same (1-P) thing.
+% So that when recipient has maximum anount of  PrEP they cannot be
+% infected by the WT.The max is so that there are no zeros in the next
+% generation matrix
+
+% Where VL{j,l}(i,1:end,k) is the infectvity for strain i in host l
+% originally infected with strain k, j is the host type being infected
+
+for i = 1:nh
     for j = 1:nh
-        for i = 1:ns
-             for k = 1:ns
-                VL{l,j}(k,1:end,i) = VL{l,j}(k,1:end,i) .* exp(-nu*tt);     
+       VL{i,j}(1,1:end,1) =  max(VL{i,j}(1,1:end,1) .* (1 - Drug_Adherance{i}(tt)), 0.001);
+       VL{i,j}(1,1:end,2) =  max(VL{i,j}(1,1:end,2) .* (1 - Drug_Adherance{i}(tt)), 0.001);
+    end
+end
+
+
+
+% Incorporate the natural mortality
+for i1 = 1:nh
+    for i2 = 1:nh
+        for j1 = 1:ns
+             for j3 = 1:ns
+                VL{i1,i2}(j3,1:end,j1) = VL{i1,i2}(j3,1:end,j1) .* exp(-nu*tt);     
              end
         end
     end
 end
 
-% VL{1}(1,1:end,1) = 3 * VL{1}(1,1:end,1);
-% VL{1}(2,1:end,1) = 1.5 * VL{1}(2,1:end,1);
-% VL{1}(1,1:end,2) = 3 * VL{1}(1,1:end,2);
-% VL{1}(2,1:end,2) = 1.5 * VL{1}(2,1:end,2);
-% VL{2}(1,1:end,1) = 1 * VL{2}(1,1:end,1);
-% VL{2}(2,1:end,1) = 1 * VL{2}(2,1:end,1);
-% VL{2}(1,1:end,2) = 1 * VL{2}(1,1:end,2);
-% VL{2}(2,1:end,2) = 1 * VL{2}(2,1:end,2);
-% VL{3}(1,1:end,1) = 1 * VL{3}(1,1:end,1);
-% VL{3}(2,1:end,1) = 1 * VL{3}(2,1:end,1);
-% VL{3}(1,1:end,2) = 1 * VL{3}(1,1:end,2);
-% VL{3}(2,1:end,2) = 1 * VL{3}(2,1:end,2);
-
-
-% In VL{k}(i,t,j) strain i initiates infection in new host, so this is
-% infctivity of strain i in host k when donor was initially infected with 
-% strain j
 
 %%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
@@ -375,11 +391,11 @@ for it = (iT0+1):lt
         discIsum = discIsum + sum(discI{ind});
     end
     
-    
-    if(it == 10000)
-        Hprops = [0.95 0.04 0.01]; % introduce PrEP
-    end
-    
+%     
+%     if(it == 10000)
+%         Hprops = [0.95 0.04 0.01]; % introduce PrEP
+%     end
+%     
 %     if(it == 30000)
 %         Hprops = [0.98 0.018 0.002]; % introduce PrEP
 %     end
@@ -465,6 +481,14 @@ end
      
      ResPrev = sum(eqlNum(2:2:end));
      
+     infectedNum = [];
+     for i = 1:nh
+         for j = 1:ns
+             infectedNum = [infectedNum; J{i}(j,end)];
+         end
+     end
+     
+     infectedAn = Ieql*(1-exp(-nu*ART))/nu;
      
 %      K(1,1) = Hprops(1) * dt * trapz( VL{1}(1,:,1) );
 %      K(1,2) = Hprops(1) * dt * trapz( VL{1}(1,:,2) );
